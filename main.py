4b0f3279b38541021589eb0d65d004be75b5f87d
Dewantoro96 <sonicsaitama34@gmail.com>
2026-06-26 14:44:04 +0700

    input subjects

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("halo github")
+def highest () :
+    subjects = {}
+    while True:
+        data1 = input("enter subject = ")
+        data2 = int(input("enter the value = "))
+        subjects.update({
+            data1 : data2
+        })
+        enough = input("is enough ? 'yes/no' =  ")
+        if enough == "no":
+            continue
+        elif enough == "yes":
+            break
+    return subjects
+
+
+
+print(highest())
